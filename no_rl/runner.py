9debe4fd1f391b5b74095a843e415c7463707e12
case547 <9521840+case547@users.noreply.github.com>
2023-04-27 18:54:39 +0100

    Create generalised baseline runner script

--- no_rl/runner.py
+++ no_rl/runner.py
@@ -1,6 +1,6 @@
+import argparse
 import csv
 import os
-import optparse
 import sys
 from pathlib import PurePath
 
@@ -19,7 +19,7 @@ import traci
 from sumolib import checkBinary
 
 
-def run(folder, csv_title, end_time=None, delta_t=1):
+def run(folder, end_time=None, delta_t=1):
     """Execute the TraCI control loop."""
     # Get traffic lights and lanes they control
     tl_ids = list(traci.trafficlight.getIDList())
@@ -29,13 +29,13 @@ def run(folder, csv_title, end_time=None, delta_t=1):
 
     # Initialise cumulative counters
     tyre_pm_cumulative = 0
-    total_arrived = 0
+    arrived_so_far = 0
     
     # Prep TensorBoard and CSV
     tb_writer = SummaryWriter(folder)
-    with open(os.path.join(folder,f"{csv_title}_{delta_t}.csv"), "w", newline="") as f:
+    with open(os.path.join(folder,f"results_{delta_t}.csv"), "w", newline="") as f:
         csv_writer = csv.writer(f)
-        csv_writer.writerow(["sim_time", "total_arrived", "tyre_pm", "tyre_pm_cumulative"])
+        csv_writer.writerow(["sim_time", "arrived", "tyre_pm"])
 
     if not end_time:
         end_time = traci.simulation.getEndTime()
@@ -53,19 +53,18 @@ def run(folder, csv_title, end_time=None, delta_t=1):
 
         # Update counters    
         tyre_pm_cumulative += tyre_pm
-        total_arrived += traci.simulation.getArrivedNumber()
+        arrived_so_far += traci.simulation.getArrivedNumber()
 
         # Log to CSV
-        with open(os.path.join(folder,f"{csv_title}_{delta_t}.csv"), "a", newline="") as f:
+        with open(os.path.join(folder,f"results_{delta_t}.csv"), "a", newline="") as f:
             csv_writer = csv.writer(f)
 
             sim_time = step * delta_t  # can also use traci.simulation.getTime()
-            csv_writer.writerow([sim_time, total_arrived, tyre_pm, tyre_pm_cumulative])
+            csv_writer.writerow([sim_time, arrived_so_far, tyre_pm, tyre_pm_cumulative])
 
         # Log to TensorBoard
-        tb_writer.add_scalar("baseline/total_arrived", total_arrived, step)
-        tb_writer.add_scalar("baseline/tyre_pm", tyre_pm, step)
-        tb_writer.add_scalar("baseline/tyre_pm_cumulative", tyre_pm_cumulative, step)
+        tb_writer.add_scalar("baseline/arrived", arrived_so_far, step)
+        tb_writer.add_scalar("baseline/tyre_pm", tyre_pm_cumulative, step)
 
         # Step simulation for delta_time seconds
         for _ in range(delta_t):
@@ -77,16 +76,16 @@ def run(folder, csv_title, end_time=None, delta_t=1):
     sys.stdout.flush()
 
 
-def get_options():
-    opt_parser = optparse.OptionParser()
-    opt_parser.add_option("--gui", action="store_true",
-                         default=False, help="run the GUI version of sumo")
-    options, _ = opt_parser.parse_args()
-    return options
+def parse_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help=".sumocfg file to run")
+    parser.add_argument("--dt", default=1, type=int, help=".sumocfg file to run")
+    parser.add_argument("--gui", action="store_true", help="run the GUI version of sumo")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    options = get_options()
+    options = parse_options()
 
     # When this script is called from the command line, it will start
     # sumo as a server, then connect and run the rest of the code
@@ -97,8 +96,7 @@ if __name__ == "__main__":
 
     # The normal way of using traci: Sumo is started as a
     # subprocess and then the python script connects and runs
-    p = PurePath(__file__)
-    traci.start([sumo_binary, "-c",
-                 os.path.join(p.parents[2],"nets","LucasAlegre","2way-single-intersection","single-intersection.sumocfg")])
+    traci.start([sumo_binary, "-c", options.file])
     
-    run(folder=p.parent, csv_title="results", delta_t=1)
+    network = PurePath(options.file).parts[-2]
+    run(folder=os.path.join("no_rl", network), delta_t=options.dt)
