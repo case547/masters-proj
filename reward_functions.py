fa67ca0003cd953c0b5db234ccd520754b966d15
case547 <9521840+case547@users.noreply.github.com>
2023-05-14 23:37:45 +0100

    Rename reward funcs and add references

--- reward_functions.py
+++ reward_functions.py
@@ -3,7 +3,7 @@ from sumo_rl import TrafficSignal
 from helper_functions import get_total_waiting_time, get_tyre_pm
 
 
-def tyre_pm(ts: TrafficSignal) -> float:
+def tyre_pm_reward(ts: TrafficSignal) -> float:
     """Return the reward as the amount of tyre PM emitted.
     
     Keyword arguments
@@ -13,20 +13,29 @@ def tyre_pm(ts: TrafficSignal) -> float:
 
 
 def diff_accum_wait_time(ts: TrafficSignal) -> float:
-    """Return the reward summing tyre PM and change in total accumulated waiting time.
+    """Return the reward as the change in total cumulative delays.
+
+    The total cumulative delay at time `t` is the sum of the accumulated wait time
+    of all vehicles present, from `t = 0` to current time step `t` in the system.
+
+    See https://arxiv.org/abs/1704.08883
     
     Keyword arguments
         ts: the TrafficSignal object
     """
-    ts_wait = sum(ts.get_accumulated_waiting_time_per_lane()) / 100.0
-    congestion_reward = ts.last_measure - ts_wait
-    ts.last_measure = ts_wait
+    # ts_wait = sum(ts.get_accumulated_waiting_time_per_lane()) / 100.0
+    # congestion_reward = ts.last_measure - ts_wait
+    # ts.last_measure = ts_wait
 
-    return tyre_pm(ts) + congestion_reward
+    # return tyre_pm(ts) + congestion_reward
+    return NotImplementedError
 
 
-def diff_wait_time(ts: TrafficSignal) -> float:
+def delta_wait_time_reward(ts: TrafficSignal) -> float:
     """Return the reward as change in total waiting time.
+
+    Waiting time is the consecutive time (in seconds) where a vehicle has been standing, exlcuding
+    voluntary stopping. See https://sumo.dlr.de/pydoc/traci._vehicle.html#VehicleDomain-getWaitingTime
     
     Keyword arguments
         ts: the TrafficSignal object
@@ -44,7 +53,7 @@ def combined_reward(ts: TrafficSignal) -> float:
     Keyword arguments
         ts: the TrafficSignal object
     """
-    return tyre_pm(ts) + diff_wait_time(ts)
+    return tyre_pm_reward(ts) + delta_wait_time_reward(ts)
 
 # Currently, trivial solution exploited where cars are just stopped;
 # Therefore need to add extra layer to prevent. Options are:
