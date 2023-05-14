5a434ec9091afc74507479b83e39fc2ff7bb4a4a
case547 <9521840+case547@users.noreply.github.com>
2023-05-14 23:38:10 +0100

    Restructure

--- sim_listener.py
+++ sim_listener.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from typing import Union
 
 import traci
+from pettingzoo.utils.conversions import aec_to_parallel_wrapper
 from torch.utils.tensorboard import SummaryWriter
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import VecEnv, VecMonitor
@@ -36,20 +37,24 @@ class SimListener(traci.StepListener):
         self.tyre_pm_cumulative = 0.
         self.arrived_so_far = 0.
 
+        self.t_step = 0.
+
+        # Get traffic signal objects
+        if isinstance(self.env, aec_to_parallel_wrapper):
+            # Results same for any/all markov vector envs, because actions are deterministic(?)
+            self.ts_dict = self.env.unwrapped.vec_envs[-1].par_env.unwrapped.env.traffic_signals
+        else:
+            self.ts_dict = self.env.get_attr("traffic_signals")[0]
+
+
     def step(self, t) -> bool:
-        sim_time = traci.simulation.getTime()
+        self.t_step += 1
 
         # In this time step
         stats = defaultdict(float)
 
         # Sum stats for traffic signals
-        if isinstance(self.env, VecEnv):
-            # Results same for any/all markov vector envs, because actions are deterministic(?)
-            ts_dict = self.env.unwrapped.vec_envs[-1].par_env.unwrapped.env.traffic_signals
-        else:
-            ts_dict = self.env.get_attr("traffic_signals")[0]
-
-        for ts in ts_dict.values():
+        for ts in self.ts_dict.values():
             stats["arrived"] += traci.simulation.getArrivedNumber()
             stats["avg_speed"] += ts.get_average_speed()
             stats["pressure"] += ts.get_pressure()
@@ -59,9 +64,9 @@ class SimListener(traci.StepListener):
 
         # Log to CSV
         if self.csv_path:
-            with open(self.csv_path, "a", encoding="ansi", newline="") as f:
+            with open(self.csv_path, "a", newline="") as f:
                 csv_writer = csv.writer(f)
-                csv_writer.writerow([sim_time] + list(stats.values()))
+                csv_writer.writerow([self.step] + list(stats.values()))
 
         # Update counters
         self.tyre_pm_cumulative += stats["tyre_pm"]
@@ -69,12 +74,12 @@ class SimListener(traci.StepListener):
 
         # Log to TensorBoard
         if self.tb_writer:
-            self.tb_writer.add_scalar("eval/arrived", self.arrived_so_far, sim_time)
-            self.tb_writer.add_scalar("eval/tyre_pm", self.tyre_pm_cumulative, sim_time)
+            self.tb_writer.add_scalar("eval/arrived", self.arrived_so_far, self.t_step)
+            self.tb_writer.add_scalar("eval/tyre_pm", self.tyre_pm_cumulative, self.t_step)
             tb_stats = list(stats.keys())
             tb_stats.remove("arrived")
             tb_stats.remove("tyre_pm")
             for s in tb_stats:
-                self.tb_writer.add_scalar(f"eval/{s}", stats[s], sim_time)
+                self.tb_writer.add_scalar(f"eval/{s}", stats[s], self.t_step)
          
         return True
